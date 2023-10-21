4782e5bbbc13cf6ed61d7dbbe96b212de7af4f4c
KseniyaLazarchik <EX_MontyPython@proton.me>
2023-10-21 15:46:11 +0200

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,11 +2,11 @@
 import sqlserverport
 
 # test data
-server = "EPPLGDAW009A"
-instance_name = "SQLEXPRESS"
+#server = "EPPLGDAW009A"
+#instance_name = "SQLEXPRESS"
 driver = "{ODBC Driver 17 for SQL Server}"
-#server = "EPPLGDAW009A\SQLEXPRESS"
-serverspec = '{0}\{1}'.format(server, sqlserverport.lookup(server, instance_name))
+server = "EPPLGDAW009A\SQLEXPRESS"
+#serverspec = '{0}\{1}'.format(server, sqlserverport.lookup(server, instance_name))
 #server = "EPPLGDAW009A\SQLEXPRESS"
 #server = "http://192.168.0.24/, 1433"
 #server = "EPPLGDAW009A, 1433"
